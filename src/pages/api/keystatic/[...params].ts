075ae35710dd1f2612143d8b0eac66e08c97160e
Jɪᴍ Rᴀʏɴᴏʀ <232726672+jimraynor88@users.noreply.github.com>
2026-09-11 01:37:39 +0200

    Delete src/pages/api/keystatic/[...params].ts

--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,9 +0,0 @@
-// src/pages/api/keystatic/[...params].ts
-import { makeRouteHandler } from '@keystatic/astro/api';
-import keystaticConfig from '../../../../keystatic.config';
-
-export const prerender = false;
-
-export const ALL = makeRouteHandler({
-  config: keystaticConfig,
-});
